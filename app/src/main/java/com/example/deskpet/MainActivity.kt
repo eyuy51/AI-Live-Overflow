ee5942999cec98cc466fc8430b70442d82f67869
eyuy51 <412343654@qq.com>
2026-07-27 13:49:18 +0800

    添加MainActivity

---
+++ app/src/main/java/com/example/deskpet/MainActivity.kt
@@ -0,0 +1,37 @@
+package com.example.deskpet
+
+import android.app.Activity
+import android.content.Intent
+import android.net.Uri
+import android.os.Build
+import android.os.Bundle
+import android.provider.Settings
+import android.widget.Toast
+import com.example.deskpet.service.ExampleOverlayService
+
+class MainActivity : Activity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M && !Settings.canDrawOverlays(this)) {
+            val intent = Intent(Settings.ACTION_MANAGE_OVERLAY_PERMISSION)
+            intent.data = Uri.parse("package:$packageName")
+            startActivity(intent)
+        } else {
+            startOverlayService()
+        }
+        finish()
+    }
+
+    private fun startOverlayService() {
+        val intent = Intent(this, ExampleOverlayService::class.java)
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            startForegroundService(intent)
+        } else {
+            startService(intent)
+        }
+        Toast.makeText(this, "悬浮窗已启动 🐙️ 2, 1000),
+            Toast.LENGTH_SHORT).show()
+    }
+}
